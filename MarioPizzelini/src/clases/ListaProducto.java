bceecacd688d64b593f076968ae5543c541294eb
LuisMaturanaC <43663191+LuisMaturanaC@users.noreply.github.com>
2019-04-11 22:00:55 -0400

    Update Luis
    Creacion mas listas

---
+++ MarioPizzelini/src/clases/ListaProducto.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clases;
+import java.util.*;
+/**
+ *
+ * @author Luis Maturana
+ */
+public class ListaProducto {
+    private ArrayList<Producto> productos;
+
+    public ListaProducto(ArrayList<Producto> productos) {
+        this.productos = productos;
+    }
+
+    public ArrayList<Producto> getProductos() {
+        return productos;
+    }
+    
+    
+}
---
+++ MarioPizzelini/src/clases/Producto.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clases;
+import java.util.*;
+/**
+ *
+ * @author Luis Maturana
+ */
+public class Producto {
+    private String id;
+    private String nombre;
+    private int precio;
+    private String descripcion;
+
+    public Producto(String id, String nombre, int precio, String descripcion) {
+        this.id = id;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.descripcion = descripcion;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+    
+}

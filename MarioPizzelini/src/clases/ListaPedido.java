d33ee3fadfcf1ed4d48388509d2179e23dfd6ee8
XavierKrostya <jav_ignacio@live.cl>
2019-04-12 00:54:12 -0300

    Update ListaPedido.java

--- MarioPizzelini/src/clases/ListaPedido.java
+++ MarioPizzelini/src/clases/ListaPedido.java
@@ -1,14 +1,53 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Clase ListaPedido, el cual manejará un ArrayList de tipo Pedido.
+ * En general se guardarán los pedidos de la pizzería.
  */
 package clases;
+import java.util.ArrayList;
 
-/**
- *
- * @author XavierKrostya
- */
 public class ListaPedido {
+    private ArrayList<Pedido> pedidos;    //ArrayList donde se guardarán los pedidos
     
+    //A continuación se el constructor de ListaPedido
+    
+    public ListaPedido(ArrayList<Pedido> pedidos) {
+        this.pedidos = pedidos;
+    }
+    
+    //Getter y setters correspondientes
+    
+    public ArrayList<Pedido> getPedidos() {
+        return pedidos;
+    }
+    
+    //Métodos de ListaPedido
+    
+    public Boolean buscarPedido(String id){
+        for(int i = 0; pedidos != null && i < pedidos.size(); i++){
+            if((pedidos.get(i)).getId().equals(id)){
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    public Boolean agregarPedido(Pedido nuevo){
+        if(pedidos.isEmpty()){
+            return pedidos.add(nuevo);
+        }
+        else{
+            return pedidos.add(nuevo);
+        }
+    }
+    
+    /*
+    public Boolean eliminarPedido(String idPedido){
+        if(pedidos.isEmpty()){
+            return false;
+        }
+        else{
+            asd
+        }
+    }
+    */
 }

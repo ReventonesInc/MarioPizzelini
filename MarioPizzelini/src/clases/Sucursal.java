5888f376384ae2db33be4e05af2e573bf0a53e1f
LuisMaturanaC <43663191+LuisMaturanaC@users.noreply.github.com>
2019-04-19 03:37:49 -0400

    Update Cbastian jeje
    uwu

--- MarioPizzelini/src/clases/Sucursal.java
+++ MarioPizzelini/src/clases/Sucursal.java
@@ -10,5 +10,31 @@ package clases;
  * @author XavierKrostya
  */
 public class Sucursal {
+    private String id;
+    private String direccion;
+    private ListaPedido pedidos;
+    private ListaCliente clientes;
+    private Inventario bodega;
+
+    public Sucursal(String id, String direccion, ListaPedido pedidos, ListaCliente clientes, Inventario bodega) {
+        this.id = id;
+        this.direccion = direccion;
+        this.pedidos = pedidos;
+        this.clientes = clientes;
+        this.bodega = bodega;
+    }
+    
+    public ListaPedido getPedidos() {
+        return pedidos;
+    }
+
+    public ListaCliente getClientes() {
+        return clientes;
+    }
+
+    public Inventario getBodega() {
+        return bodega;
+    }    
+    
     
 }

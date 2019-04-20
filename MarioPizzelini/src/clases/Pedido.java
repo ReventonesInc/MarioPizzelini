ee1ce794a137c107bcdc7da74405f364cbf91b2e
LuisMaturanaC <43663191+LuisMaturanaC@users.noreply.github.com>
2019-04-19 21:30:26 -0400

    Update pedido cbastian
    c b a s t i a n

--- MarioPizzelini/src/clases/Pedido.java
+++ MarioPizzelini/src/clases/Pedido.java
@@ -5,68 +5,36 @@
 package clases;
 
 public class Pedido {
-    private String id;                          //id del pedido
+    private String idPedido;                          //id del pedido
     private Cliente clienteReferenciado;        //cliente que ordenó el pedido
-    private Pizza pizzaReferenciada;            //Pizza que puede escoger el cliente
-    private Producto productoReferenciado;      //Producto que puede escoger el cliente
+    private Pizza[] pizzasPedidas;            //Pizza que puede escoger el cliente
+    private Bebestible[] bebestiblesPedidos;      //Producto que puede escoger el cliente
     
     //A continuación constructores de pedido
-    /*
-    //Cuando el cliente pide: Promo, Producto, Pizza
-    public Pedido(String id, Cliente clienteReferenciado, Pizza pizzaReferenciada, Promocion promoReferenciada, Producto productoReferenciado){
-        this.id = id;
-        this.clienteReferenciado = clienteReferenciado;
-        this.pizzaReferenciada = pizzaReferenciada;
-        this.promoReferenciada = promoReferenciada;
-        this.productoReferenciado = productoReferenciado;
-    }
-    
-    //Cuando el cliente pide: Promo, Producto
-    public Pedido(String id, Cliente clienteReferenciado, Promocion promoReferenciada, Producto productoReferenciado){
-        this.id = id;
-        this.clienteReferenciado = clienteReferenciado;
-        this.promoReferenciada = promoReferenciada;
-        this.productoReferenciado = productoReferenciado;
-    }
-    
-    //Cuando el cliente pide: Promo
-    public Pedido(String id, Cliente clienteReferenciado,Promocion promoReferenciada){
-        this.id = id;
-        this.clienteReferenciado = clienteReferenciado;
-        this.promoReferenciada = promoReferenciada;
-    }
     
     //Cuando el cliente pide: Pizza
-    public Pedido(String id, Cliente clienteReferenciado, Pizza pizzaReferenciada){
-        this.id = id;
+    public Pedido(String idPedido, Cliente clienteReferenciado, int cantPizzas){
+        this.idPedido = idPedido;
         this.clienteReferenciado = clienteReferenciado;
-        this.pizzaReferenciada = pizzaReferenciada;
+        this.pizzasPedidas = new Pizza[cantPizzas];
     }
     
     //Cuando el cliente pide: Producto, Pizza
-    public Pedido(String id, Cliente clienteReferenciado, Producto productoReferenciado, Pizza pizzaReferenciada){
-        this.id = id;
+    public Pedido(String id, Cliente clienteReferenciado, int cantPizzas, int cantBebestibles){
+        this.idPedido = idPedido;
         this.clienteReferenciado = clienteReferenciado;
-        this.productoReferenciado = productoReferenciado;
-        this.pizzaReferenciada = pizzaReferenciada;
+        this.pizzasPedidas = new Pizza[cantPizzas];
+        this.bebestiblesPedidos = new Bebestible[cantBebestibles];
     }
     
-    //Cuando el cliente pide: Promo, Pizza
-    public Pedido(String id, Cliente clienteReferenciado, Promocion promoReferenciada, Pizza pizzaReferenciada){
-        this.id = id;
-        this.clienteReferenciado = clienteReferenciado;
-        this.promoReferenciada = promoReferenciada;
-        this.pizzaReferenciada = pizzaReferenciada;
-    }
-    */
     //Getters y setters correspondientes
-    
-    public String getId() {
-        return id;
+
+    public String getIdPedido() {
+        return idPedido;
     }
 
-    public void setId(String id) {
-        this.id = id;
+    public void setIdPedido(String idPedido) {
+        this.idPedido = idPedido;
     }
 
     public Cliente getClienteReferenciado() {
@@ -77,19 +45,20 @@ public class Pedido {
         this.clienteReferenciado = clienteReferenciado;
     }
 
-    public Pizza getPizzaReferenciada() {
-        return pizzaReferenciada;
+    public Pizza[] getPizzasPedidas() {
+        return pizzasPedidas;
     }
 
-    public void setPizzaReferenciada(Pizza pizzaReferenciada) {
-        this.pizzaReferenciada = pizzaReferenciada;
+    public void setPizzasPedidas(Pizza[] pizzasPedidas) {
+        this.pizzasPedidas = pizzasPedidas;
     }
 
-    public Producto getProductoReferenciado() {
-        return productoReferenciado;
+    public Bebestible[] getBebestiblesPedidos() {
+        return bebestiblesPedidos;
     }
 
-    public void setProductoReferenciado(Producto productoReferenciado) {
-        this.productoReferenciado = productoReferenciado;
+    public void setBebestiblesPedidos(Bebestible[] bebestiblesPedidos) {
+        this.bebestiblesPedidos = bebestiblesPedidos;
     }
+    
 }

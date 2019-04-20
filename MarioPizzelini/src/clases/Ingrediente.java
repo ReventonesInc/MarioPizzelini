d2a0f414b308cc05663bb5869b44c2e556fd6b1f
XavierKrostya <jav_ignacio@live.cl>
2019-04-19 22:46:44 -0300

    Update Ingrediente.java

--- MarioPizzelini/src/clases/Ingrediente.java
+++ MarioPizzelini/src/clases/Ingrediente.java
@@ -5,12 +5,15 @@
  */
 package clases;
 
-public class Ingrediente extends Producto {
+public class Ingrediente {
     private String idIngrediente;
+    private String nombre;
+    private int precio;
     
-    public Ingrediente(String idIngrediente, String nombre, int precio, String descripcion) {
-        super(nombre, precio, descripcion);
+    public Ingrediente(String idIngrediente, String nombre, int precio) {
         this.idIngrediente = idIngrediente;
+        this.nombre = nombre;
+        this.precio = precio;
     }
 
     public String getIdIngrediente() {
@@ -19,5 +22,21 @@ public class Ingrediente extends Producto {
 
     public void setIdIngrediente(String idIngrediente) {
         this.idIngrediente = idIngrediente;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
     }
 }

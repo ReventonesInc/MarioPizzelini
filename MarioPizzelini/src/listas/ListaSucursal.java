c91967372a327748447b31b853d5f12a08f17c85
XavierKrostya <jav_ignacio@live.cl>
2019-04-19 23:54:21 -0300

    Update ListaSucursal.java

--- MarioPizzelini/src/listas/ListaSucursal.java
+++ MarioPizzelini/src/listas/ListaSucursal.java
@@ -4,6 +4,8 @@
  * and open the template in the editor.
  */
 package listas;
+import clases.Bebestible;
+import clases.Ingrediente;
 import clases.Sucursal;
 import java.util.ArrayList;
 
@@ -32,6 +34,74 @@ public class ListaSucursal {
     public Boolean eliminarSucursal(Sucursal eliminar){
         if(sucursales.contains(eliminar)){
             return sucursales.remove(eliminar);
+        }
+        return false;
+    }
+    
+    public Boolean agregarIngrediente(String rut, Ingrediente nuevo){
+        if(!sucursales.isEmpty()){
+            for(int i = 0; sucursales != null && i < sucursales.size(); i++){
+                if((sucursales.get(i)).getRut().equals(rut)){
+                    return sucursales.get(i).agregarIngrediente(nuevo);
+                }
+            }
+        }
+        return false;
+    }
+    
+    public Boolean eliminarIngrediente(String rut, Ingrediente eliminar){
+        if(!sucursales.isEmpty()){
+            for(int i = 0; sucursales != null && i < sucursales.size(); i++){
+                if((sucursales.get(i)).getRut().equals(rut)){
+                    return sucursales.get(i).eliminarIngrediente(eliminar);
+                }
+            }
+        }
+        return false;
+    }
+    
+    public Boolean modificarIngrediente(String rut, Ingrediente modificar, Ingrediente modificado){
+        if(!sucursales.isEmpty()){
+            for(int i = 0; sucursales != null && i < sucursales.size(); i++){
+                if((sucursales.get(i)).getRut().equals(rut)){
+                    sucursales.get(i).modificarIngrediente(modificar, modificado);
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    
+    public Boolean agregarBebestible(String rut, Bebestible nuevo){
+        if(!sucursales.isEmpty()){
+            for(int i = 0; sucursales != null && i < sucursales.size(); i++){
+                if((sucursales.get(i)).getRut().equals(rut)){
+                    return sucursales.get(i).agregarBebestible(nuevo);
+                }
+            }
+        }
+        return false;
+    }
+    
+    public Boolean eliminarBebestible(String rut, Bebestible eliminar){
+        if(!sucursales.isEmpty()){
+            for(int i = 0; sucursales != null && i < sucursales.size(); i++){
+                if((sucursales.get(i)).getRut().equals(rut)){
+                    return sucursales.get(i).eliminarBebestible(eliminar);
+                }
+            }
+        }
+        return false;
+    }
+    
+    public Boolean modificarBebestible(String rut, Bebestible modificar, Bebestible modificado){
+        if(!sucursales.isEmpty()){
+            for(int i = 0; sucursales != null && i < sucursales.size(); i++){
+                if((sucursales.get(i)).getRut().equals(rut)){
+                    sucursales.get(i).modificarBebestible(modificar, modificado);
+                    return true;
+                }
+            }
         }
         return false;
     }

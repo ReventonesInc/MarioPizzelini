60801e4d453f4d564ce2df7ea0a15b637b2ce685
XavierKrostya <jav_ignacio@live.cl>
2019-05-04 22:01:23 -0300

    Update Cliente.java

--- MarioPizzelini/src/clases/Cliente.java
+++ MarioPizzelini/src/clases/Cliente.java
@@ -59,13 +59,4 @@ public class Cliente {
     public void setTelefono(String telefono) {
         this.telefono = telefono;
     }
-
-    /*public String getDireccion() {
-        return direccion;
-    }
-
-    public void setDireccion(String direccion) {
-        this.direccion = direccion;
-    }*/
-    
 }
